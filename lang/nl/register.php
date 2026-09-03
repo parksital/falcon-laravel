1a82da17df267e22463cb84e96dc8ff13b4f5040
Parvin Sital <rsital@icloud.com>
2026-09-03 09:49:57 +0200

    Add Dutch auth and overview copy

--- lang/nl/register.php
+++ lang/nl/register.php
@@ -2,17 +2,17 @@
 
 return [
     'title' => 'Account aanmaken',
-    'heading' => 'Maak een account aan',
-    'description' => 'Vul hieronder je gegevens in om je account aan te maken',
+    'heading' => 'Maak je bedrijfsaccount aan',
+    'description' => 'Beheer je diensten en maak ze vindbaar.',
     'name_label' => 'Naam',
     'name_placeholder' => 'Volledige naam',
     'email_label' => 'E-mailadres',
     'email_placeholder' => 'naam@voorbeeld.nl',
     'password_label' => 'Wachtwoord',
-    'password_placeholder' => 'Wachtwoord',
+    'password_placeholder' => '',
     'password_confirmation_label' => 'Bevestig wachtwoord',
-    'password_confirmation_placeholder' => 'Bevestig wachtwoord',
-    'submit' => 'Maak account aan',
+    'password_confirmation_placeholder' => '',
+    'submit' => 'Ga verder',
     'login_prompt' => 'Heb je al een account?',
     'login_link' => 'Log in',
 ];
